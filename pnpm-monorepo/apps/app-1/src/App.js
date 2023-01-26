fb9c19176c4c8851e1332a63a53b8b2f381a5b47
Unais Vayani <unais.vayani@ibex.co>
2023-01-26 16:29:50 +0500

    Using shared component in app 1

--- pnpm-monorepo/apps/app-1/src/App.js
+++ pnpm-monorepo/apps/app-1/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "shared-ui";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Running App 1</p>
+        <Button/>
       </header>
     </div>
   );
